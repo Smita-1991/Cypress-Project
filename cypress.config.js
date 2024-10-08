3ffb14eb0a1d35d0c15c58c5c5a889e54230df8f
Smita-1991 <autismita5@gmail.com>
2024-10-08 13:37:56 -0400

    Created new files

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+
+    experimentalSessionAndOrigin: true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      screenshotOnRunFailure=true;
+    },
+  },
+});
